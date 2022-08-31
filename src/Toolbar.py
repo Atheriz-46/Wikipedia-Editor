634bb37b5e27b5b4b285976f998506c280f5941d
pushpit-sri <sonika@Sonikas-MacBook-Air-2.local>
2022-08-31 16:41:45 +0530

    Modified Toolbar (added back and render)

--- src/Toolbar.py
+++ src/Toolbar.py
@@ -93,7 +93,7 @@ class Toolbar(tk.Frame):
 
 		self.manager = guimanager
 
-		self.btn_back = tk.Button(master = self, text = " < ")
+		self.btn_back = tk.Button(master = self, text = " < ", command = self.manager.back())
 		self.btn_back.grid(row = 0, column = 0, sticky = "e", padx = 10, pady = 10)
 
 		self.btn_home = tk.Button(master = self, text = "Home", command = self.manager.switchToHomePage())
@@ -103,8 +103,9 @@ class Toolbar(tk.Frame):
 		self.frm_edit = tk.Frame(master = self)
 		self.frm_view = tk.Frame(master = self)
 
-		self.btn_render = tk.Button(master = self.frm_edit, text = "Render")
+		self.btn_render = tk.Button(master = self.frm_edit, text = "Render", command = self.manager.frameEditor.render())
 		self.btn_render.pack(side = tk.LEFT) 
+
 		self.btn_save = tk.Button(master = self.frm_edit, text = "Save", command = self.manager.switchToViewer())
 		self.btn_save.pack(side = tk.LEFT)
 
@@ -114,6 +115,12 @@ class Toolbar(tk.Frame):
 		self.frm_home.grid(row = 0, column = 2, sticky = "news")
 		self.frm_view.grid(row = 0, column = 2, sticky = "news")
 		self.frm_edit.grid(row = 0, column = 2, sticky = "news")
+
+# window = tk.Tk()
+
+# tb = Toolbar(window,window)
+
+# tb.pack()
 
 
 # mainApplication = GUIManager()
