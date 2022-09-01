8740e84eac8879acaf0a6c76a7173d8ee738992e
HrshWardhan <hwardhan337@gmail.com>
2022-09-01 21:43:54 +0530

    icon added

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,15 @@
 from AskDirectory import AskDirectory 
 from GUIManager   import GUIManager
-
+import os
+import tkinter as tk 
 class Bstring:
     directory = ""
-
+ 
+iconDirectory = os.path.join(os.getcwd(),"assets","icon.png")
 directory = Bstring()
 ad = AskDirectory(directory)
+ad.tk.call('wm', 'iconphoto', ad._w, tk.PhotoImage(file=iconDirectory))
 ad.mainloop()
 mainApplication = GUIManager(directory.directory)
+mainApplication.tk.call('wm', 'iconphoto', mainApplication._w, tk.PhotoImage(file=iconDirectory))
 mainApplication.mainloop()
