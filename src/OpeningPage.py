b771cf589f1fc21f4890bde9baf4035b0f2368c2
Atheriz-46 <atherissummers@gmail.com>
2022-09-01 23:56:11 +0530

    Fixed bugs

--- src/OpeningPage.py
+++ src/OpeningPage.py
@@ -9,5 +9,5 @@ class OpeningPage:
     def generate(self):
         file_list = glob(join(self.fm.currentDirectory,'*.md'))
         content = '# Opening Page \n'
-        content = content + '\n'.join(f"- [{name}]({name})" for name in file_list)
+        content = content + '\n'.join(f"- [{name.split('/')[-1][:-3]}]({name})" for name in file_list)
         self.fm.saveTo("HomePage.md",content)
--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,10 @@
 from GUIManager   import GUIManager
 import os
 import tkinter as tk 
-from tkinter import filedialog 
+from tkinter import PhotoImage, filedialog 
  
 iconDirectory = os.path.join(os.getcwd(),"assets","icon.png")
-path_work = filedialog.askdirectory() 
-if not isinstance(path_work,tuple):
-    mainApplication = GUIManager(path_work)
-    mainApplication.tk.call('wm', 'iconphoto', mainApplication._w, tk.PhotoImage(file=iconDirectory))
-    mainApplication.mainloop()
+path_work = filedialog.askdirectory(initialdir='./') 
+path_work = os.path.relpath(path_work,os.getcwd())
+mainApplication = GUIManager(path_work)
+mainApplication.mainloop()
