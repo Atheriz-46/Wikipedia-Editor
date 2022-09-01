172bd7d65fd2e34d3d28f13b40d14dc8571b7bec
Atheriz-46 <atherissummers@gmail.com>
2022-09-01 21:42:30 +0530

    [FIxed] pesistent formatting issue

--- src/Viewer.py
+++ src/Viewer.py
@@ -5,9 +5,14 @@ class Viewer(tk.Frame):
     def changeViewerArticle(self,articleName):
         # TODO Markdown parser call and change contents 
         contents = self.manager.fm.fetchFrom(articleName)
-        self.textFrame.config(state='normal')
-        self.textFrame.delete('1.0',tk.END)
+        self.textFrame.destroy()
+        # self.textFrame.config(state='normal')
+        # for tag in self.textFrame.tag_names():
+            # self.textFrame.tag_remove(tag, 1.0, tk.END)
+        # self.textFrame.delete('1.0',tk.END)
         self.parser.parse(contents)
+        self.textFrame = tk.Text(master=self)
+        self.textFrame.pack(fill="both",expand=True)
         self.textFrame = self.parser.getFrame(self.textFrame)
         self.textFrame.config(state='disabled')
          
